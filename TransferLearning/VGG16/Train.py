78b3e5fb86f2b8b328b70ca2e31f92347920d0a9
pmartyn <phil.martyn@aviso.io>
2019-08-18 15:44:26 +0100

    Fixed test

--- TransferLearning/VGG16/Train.py
+++ TransferLearning/VGG16/Train.py
@@ -1,28 +1,32 @@
 # Borrowed heavily from https://github.com/marciahon29/Ryerson_MRP
+# and https://blog.keras.io/building-powerful-image-classification-models-using-very-little-data.html
+import io
 
 import numpy as np
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dropout, Flatten, Dense
+from keras.utils import normalize
+from keras.optimizers import Adam
+from keras.layers import Dropout, Flatten, Dense, BatchNormalization
 from keras import applications
 from sklearn.preprocessing import LabelBinarizer
 import pickle
-
+from keras.regularizers import l2
 
 # dimensions of our images.
-img_width, img_height = 240, 240
+img_width, img_height = 150, 150
 
 # Paths to model output and data
 top_model_weights_path = '/Users/pmartyn/PycharmProjects/Thesis/Output/bottleneck_weights.h5'
 model_path = '/Users/pmartyn/PycharmProjects/Thesis/Output/bipolar.model'
-train_data_dir = '/Users/pmartyn/PycharmProjects/Thesis/Data/converted/train'
-validation_data_dir = '/Users/pmartyn/PycharmProjects/Thesis/Data/converted/validation'
+train_data_dir = '/Users/pmartyn/PycharmProjects/Thesis/TransferLearning/Data/Crossfolds/Fold1/converted/train'
+validation_data_dir = '/Users/pmartyn/PycharmProjects/Thesis/TransferLearning/Data/Crossfolds/Fold1/converted/validation'
 # Hyperparameters inputs relating to data
-nb_train_samples = 6112
-nb_validation_samples = 1440
+nb_train_samples = 4520
+nb_validation_samples = 1280
 init_lr = 1e-3
 epochs = 50
-batch_size = 40
+batch_size = 16
 data_array = []
 labels_array = []
 
@@ -33,20 +37,22 @@ def gen_and_save_bottleneck_features():
     imagegen = ImageDataGenerator(rescale=1. / 255)
 
     # build the VGG16 network
-    model = applications.VGG16(include_top=False, weights='imagenet', input_shape=(240,240,3))
+    model = applications.VGG16(include_top=False, weights='imagenet')
     print("Model created. ", model)
+
     generator = imagegen.flow_from_directory(
         train_data_dir,
         target_size=(img_width, img_height),
         batch_size=batch_size,
         class_mode=None,
         shuffle=False)
+
     print("Generator created. ", generator)
     bottleneck_features_train = model.predict_generator(
         generator, nb_train_samples // batch_size)
     print("bottleneck_features_train created. ", bottleneck_features_train)
 
-    np.save(open('bottleneck_features_train.npy', 'w'),
+    np.save('bottleneck_features_train.npy',
             bottleneck_features_train)
 
     generator = imagegen.flow_from_directory(
@@ -57,52 +63,53 @@ def gen_and_save_bottleneck_features():
         shuffle=False)
     bottleneck_features_validation = model.predict_generator(
         generator, nb_validation_samples // batch_size)
-    np.save(open('bottleneck_features_validation.npy', 'w'),
+    np.save('bottleneck_features_validation.npy',
             bottleneck_features_validation)
 
 
 # Train the VGG16 fully connected layer using the data.
 def train_top_model():
-
-    train_data = np.load(open('bottleneck_features_train.npy'))
+    train_data = np.load(open('bottleneck_features_train.npy', 'rb'))
     train_labels = np.array(
-        [0] * (nb_train_samples / 2) + [1] * (nb_train_samples / 2))
+        [0] * int(nb_train_samples / 2) + [1] * int(nb_train_samples / 2))
 
     print("Training data. ", train_data.size)
     print("Training labels. ", train_labels.size)
+    print("Training labels. ", train_labels)
 
     lb = LabelBinarizer()
     training_labels = lb.fit_transform(train_labels)
 
     print("Training labels post labeliser. ", training_labels.size)
 
-    validation_data = np.load(open('bottleneck_features_validation.npy'))
+    validation_data = np.load(open('bottleneck_features_validation.npy', 'rb'))
     validation_labels = np.array(
-        [0] * (nb_validation_samples / 2) + [1] * (nb_validation_samples / 2))
+        [0] * int(nb_validation_samples / 2) + [1] * int(nb_validation_samples / 2))
 
-    val_labels = lb.transform(validation_labels)
+    val_labels = lb.fit_transform(validation_labels)
 
-    # Output the pickle file for use later in prediction
-    f = open("/Users/pmartyn/PycharmProjects/Thesis/Output/bipolar_lb.pickle", "wb")
-    f.write(pickle.dumps(lb))
-    f.close()
+    print("Validation shape ", validation_data.shape[1])
 
     model = Sequential()
     model.add(Flatten(input_shape=train_data.shape[1:]))
-    model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(1, activation='sigmoid'))
+    model.add(Dense(256, activation='relu', kernel_regularizer=l2(0.0005), bias_regularizer=l2(0.0005)))
+    model.add(BatchNormalization())
+    # model.add(Dropout(0.5))
+    model.add(Dense(1, activation='sigmoid', kernel_regularizer=l2(0.0005), bias_regularizer=l2(0.0005)))
 
     model.compile(optimizer='rmsprop',
                   loss='binary_crossentropy', metrics=['accuracy'])
 
-    model.fit(train_data, train_labels,
-              epochs=epochs,
-              batch_size=batch_size,
-              validation_data=(validation_data, val_labels))
+    history = model.fit(train_data, training_labels,
+                        epochs=epochs,
+                        batch_size=batch_size,
+                        validation_data=(validation_data, val_labels))
     model.save_weights(top_model_weights_path)
     model.save(model_path)
 
+    with open('/Users/pmartyn/PycharmProjects/Thesis/Output/history', 'wb') as file_pi:
+        pickle.dump(history.history, file_pi)
 
-# gen_and_save_bottleneck_features()
+
+gen_and_save_bottleneck_features()
 train_top_model()
--- test/VGG16/predict_test.py
+++ test/VGG16/predict_test.py
@@ -4,16 +4,20 @@ from src.predictor.image.prepDataIO import prep_data
 
 
 # Test that prep_data stores JPG files in the tmp directory
-nii_output_dir = "/Users/pmartyn/PycharmProjects/Thesis/tmp/proc_img"
+nii_test_file = "/Users/pmartyn/PycharmProjects/Thesis/test/VGG16/test-file/sub-60001_anat_sub-60001_T1w.nii"
+nii_base_out_path = "/Users/pmartyn/PycharmProjects/Thesis/test/VGG16/tmp/12345/"
+nii_output_dir = nii_base_out_path + "proc_img"
 
 
 def number_of_files():
     return len(os.listdir(nii_output_dir))
 
 
-# assert number_of_files() == 0
+if not os.path.exists(nii_base_out_path):
+    os.makedirs(nii_base_out_path)
 
-prep_data("/Users/pmartyn/PycharmProjects/Thesis/TransferLearning/Data/prediction-data/test/sub-60001_anat_sub-60001_T1w.nii")
+prep_data(nii_test_file,
+          nii_base_out_path)
 #
 # print("Number of files ", number_of_files())
 
@@ -22,7 +26,7 @@ assert number_of_files() == 10
 assert all(file.endswith('.jpg') for file in os.listdir(nii_output_dir))
 
 try:
-    image_data = predict()
+    image_data = predict(nii_base_out_path)
     print(image_data)
     assert all(file["class-label"] == 'Bipolar' for file in image_data)
     assert all(file["filename"] is not None for file in image_data)
